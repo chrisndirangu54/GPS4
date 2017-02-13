dd6b892a7f66ad4a54bc82c5a1cb4712536bb316
cisco <mutegison2@gmail.com>
2017-02-13 10:43:18 +0300

    Adding global constants for maintainability

---
+++ app/src/main/java/ke/co/alandick/gps4/GlobalConstants.java
@@ -0,0 +1,14 @@
+package ke.co.alandick.gps4;
+
+/**
+ * Created by cisco on 2/13/17.
+ */
+
+public class GlobalConstants {
+
+    public static final String endpoint = "http://d6bc1809.ngrok.io/CarTracker/geoLocation?";
+    public static final long delay = 1000 * 30;
+    public static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 0; // 1 meters
+    public static final String driverLoginendpoint = "http://d6bc1809.ngrok.io/CarTracker/user/driverLogin?";
+
+}
